8ce4a2c96bce9a9a036f2d93b20dd897c3d903bc
karthikshanbhag <karthikshanbhag969@gmail.com>
2023-08-12 21:34:47 +0530

    nav added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import AllRoutes from './routes/AllRoutes';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
       <div>
-        <AllRoutes />
+        {/* <AllRoutes /> */}
+        <Navbar />
       </div>
     </>
   );
