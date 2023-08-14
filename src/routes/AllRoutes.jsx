88a6ab04e4a63bc339c3bc1512a25172bec5d6a7
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-08-14 10:01:03 +0530

    Update AllRoutes.jsx

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -2,29 +2,13 @@
 import { Route, Routes } from 'react-router-dom'
 import Homepage from '../HomePage/Homepage'
 
-import Home from '../components/Home'
-const Paths = [
-    { path: '/', element: <Homepage /> },
-]
-
-
 import Login from '../Pages/Login'
 
-// const Paths = [
-//     { path: '/', element: <Homepage /> },
-// ]
 
 
 const AllRoutes = () => {
     return (
         <Routes>
-
-            {/* <Route exact path='/' element={<Homepage />}></Route> */}
-            {
-                Paths.map(path => {
-                    return <Route exact path={path.path} element={path.element}></Route>
-                })
-            }
 
             <Route exact path='/' element={<Homepage />}></Route>
 
