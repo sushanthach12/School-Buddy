a5532d21cd7383aff7ff460197b0735242805782
karthikshanbhag <karthikshanbhag969@gmail.com>
2023-08-15 12:43:18 +0530

    invoice generator

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -7,6 +7,7 @@ import Signup from '../Pages/Signup'
 import Profile from '../Pages/Dashboard/Profile'
 import Analytics from '../Pages/Analytics'
 import LinkGenerator from '../Pages/LinkGenerator'
+import Invoicegenerator from '../Pages/Invoicegenerator'
 
 
 const AllRoutes = () => {
@@ -19,6 +20,8 @@ const AllRoutes = () => {
             <Route exact path='/dashboard' element={<Profile />}></Route>
             <Route exact path='/analytics' element={<Analytics />}></Route>
             <Route exact path='/link-generator' element={<LinkGenerator />}></Route>
+            <Route exact path='/invoice-generator' element={<Invoicegenerator />}></Route>
+
 
         </Routes>
     )
