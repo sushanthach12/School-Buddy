30fbd10076ff8fa0ce7ac00deed99c87d49eae0f
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-08-16 21:44:48 +0530

    Update AllRoutes.jsx

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -8,12 +8,12 @@ import Profile from '../Pages/Dashboard/Profile'
 import Analytics from '../Pages/Analytics'
 import LinkGenerator from '../Pages/LinkGenerator'
 import Invoicegenerator from '../Pages/Invoicegenerator'
+import InvoiceHistory from '../Pages/InvoiceHistory'
 
 
 const AllRoutes = () => {
     return (
         <Routes>
-
             <Route exact path='/' element={<Homepage />}></Route>
             <Route exact path='/login' element={<Login />}></Route>
             <Route exact path='/signup' element={<Signup />}></Route>
@@ -21,8 +21,7 @@ const AllRoutes = () => {
             <Route exact path='/analytics' element={<Analytics />}></Route>
             <Route exact path='/link-generator' element={<LinkGenerator />}></Route>
             <Route exact path='/invoice-generator' element={<Invoicegenerator />}></Route>
-
-
+            <Route exact path='/invoice-history' element={<InvoiceHistory />}></Route>
         </Routes>
     )
 }
