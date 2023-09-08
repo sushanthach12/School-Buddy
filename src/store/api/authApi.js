d772d95dd811e5d25ef08c5b2885d5e5f213e78f
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-09-08 11:20:01 +0530

    Update authApi.js

--- src/store/api/authApi.js
+++ src/store/api/authApi.js
@@ -8,31 +8,31 @@ export const authApi = createApi({
             query: (credentials) => ({
                 url: '/register',
                 method: 'POST',
-                headers: { 'content-type': 'application/json'},
+                headers: { 'content-type': 'application/json' },
                 body: { name: credentials.name, email: credentials.email, password: credentials.password }
             }),
-    transformResponse: (response) => {
-        return {
-            ...response?.user,
-            token: response?.token,
-        }
-    }
-}),
-    loginUser: builder.mutation({
-        query: (credentials) => ({
-            url: '/login',
-            method: 'POST',
-            headers: { 'Content-type': 'application/json'},
-            body: { email: credentials.email, password: credentials.password }
-        }),
-        transformResponse: (response) => {
-            return {
-                ...response?.user,
-                ...response?.upload,
-                token: response?.token
+            transformResponse: (response) => {
+                return {
+                    ...response?.user,
+                    token: response?.token,
+                }
             }
-        }
-    })
+        }),
+        loginUser: builder.mutation({
+            query: (credentials) => ({
+                url: '/login',
+                method: 'POST',
+                headers: { 'content-type': 'application/json' },
+                body: { email: credentials.email, password: credentials.password }
+            }),
+            transformResponse: (response) => {
+                return {
+                    ...response?.user,
+                    ...response?.upload,
+                    token: response?.token
+                }
+            }
+        })
     })
 })
 
