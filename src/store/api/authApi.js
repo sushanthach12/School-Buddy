1fffcc2d22937dcbe09f9bca353393cb5d7bdecb
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-09-09 08:51:12 +0530

    Update authApi.js

--- src/store/api/authApi.js
+++ src/store/api/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 export const authApi = createApi({
     reducerPath: "authApi",
     baseQuery: fetchBaseQuery({ 
-        baseUrl: `$https://school-buddy-backend.vercel.app/api/auth`,
+        baseUrl: `https://school-buddy-backend.vercel.app/api/auth`,
     }),
     endpoints: (builder) => ({
         registerUser: builder.mutation({
