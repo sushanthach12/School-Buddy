abd2426fbf8d38bd8f22513a4f72372274de1669
Sushanth Acharya <84125038+sushanthach12@users.noreply.github.com>
2023-09-17 12:30:18 +0530

    Update invoiceApi.js

--- src/store/api/invoiceApi.js
+++ src/store/api/invoiceApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react'
 
 export const invoiceApi = createApi({
     reducerPath: "invoiceApi",
-    baseQuery: fetchBaseQuery({ baseUrl: `https://school-buddy-backend.vercel.app/api/invoice` }),
+    baseQuery: fetchBaseQuery({ baseUrl: `http://64.227.150.164/api/invoice` }),
     // baseQuery: fetchBaseQuery({ baseUrl: `http://localhost:8080/api/invoice` }),
     endpoints: (builder) => ({
         createInvoice: builder.mutation({
